9c12da017a440cdd39b91ab90eddbdab7240f02e
BipulRahi <bipulrahi18@gmail.com>
2026-04-21 00:22:58 +0530

    render

--- Backend/main.py
+++ Backend/main.py
@@ -135,9 +135,19 @@ async def generate_caption(file: UploadFile = File(...)):
         )
 
 
+# if __name__ == "__main__":
+#     import uvicorn
+
+#     uvicorn.run(
+#         "main:app",
+#         host="0.0.0.0",
+#         port=8000,
+#         reload=True
+#     )
+
 if __name__ == "__main__":
-    import uvicorn
     import os
+    import uvicorn
 
     port = int(os.environ.get("PORT", 8000))
 
